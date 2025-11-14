a31529b31f484c9812ace360cd37d4359536af2f
ejamesthedon1-del <ejamesthedon1@gmail.com>
2025-11-14 14:51:04 -0600

    Align first blue card with '4' in headline on mobile view

--- src/components/SlidingInfoSection.tsx
+++ src/components/SlidingInfoSection.tsx
@@ -18,7 +18,7 @@ export function SlidingInfoSection() {
 
         {/* Scrollable Cards */}
         <div className="relative mt-2">
-          <div className="overflow-x-auto scrollbar-hide snap-x snap-mandatory pl-4 md:pl-0">
+          <div className="overflow-x-auto scrollbar-hide snap-x snap-mandatory pl-4 md:pl-0 -mx-4 md:mx-0">
             <div className="flex gap-6 pb-4">
               {/* Card 1: Blue Card Bottom Text */}
               <div className="flex-shrink-0 w-[68vw] md:w-[320px] snap-start">
